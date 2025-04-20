6ae57fa1268e4a3fc8ae30396d1bd17b95364bb8
3jun <kk3june@gmail.com>
2025-04-21 03:00:42 +0900

    feat. cart model discount 기능 구현

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,21 +1,37 @@
 import { CartItem, Coupon } from "../../types";
+import { getTotalDiscount } from "../utils/discount";
+
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const itemTotalPrice = item.product.price * item.quantity;
+  
+  return itemTotalPrice * (1 - getMaxApplicableDiscount(item))
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const maxDiscount: number = item.product.discounts.reduce((maxDiscount, discount) => {
+    if(discount.quantity <= item.quantity) {
+      maxDiscount = discount.rate;
+    }
+    return maxDiscount;
+  }, 0)
+  return maxDiscount;
 };
 
 export const calculateCartTotal = (
   cart: CartItem[],
   selectedCoupon: Coupon | null
 ) => {
+  const totalBeforeDiscount = cart.reduce((sum, cur) => sum += cur.product.price * cur.quantity, 0)
+  
+  const totalDiscount = getTotalDiscount({ cart, coupon: selectedCoupon, totalBeforeDiscount })
+
+  const totalAfterDiscount = totalBeforeDiscount - totalDiscount
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
   };
 };
 
