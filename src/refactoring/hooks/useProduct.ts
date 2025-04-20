e2727a1d11bb0457505879721b1d206ba9cad51a
상은 <1-blue98@naver.com>
2025-04-20 12:21:32 +0900

    ✨: useProduct 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,22 @@
 import { useState } from "react";
-import { Product } from "../../types.ts";
+import type { Product } from "#src/types";
 
+// 엔티티를 다루는 훅
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  /** 상품 추가 */
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+  /** 상품 수정 */
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prevProducts) => prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)));
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    addProduct,
+    updateProduct,
   };
 };
