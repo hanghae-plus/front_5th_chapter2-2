731adb1bc47bd49a1f096d01528b5e29125b5e81
hsw824 <ghdtjddn0824@naver.com>
2025-04-21 01:19:48 +0900

    feat: useCart 중간 추가

--- src/refactoring/hooks/useCart.ts
+++ src/refactoring/hooks/useCart.ts
@@ -7,11 +7,50 @@ export const useCart = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
 
-  const addToCart = (product: Product) => {};
+  const addToCart = (product: Product) => {
+    // 리팩토링 필요
+    const targetProduct = cart.find((item) => item.product.id === product.id);
+    if (!targetProduct) {
+      setCart((prev) => [
+        ...prev,
+        {
+          product: { ...product, stock: product.stock - 1 },
+          quantity: 1,
+        },
+      ]);
+      return;
+    }
+    setCart(
+      cart.map((item) => {
+        if (item.product.id === product.id) {
+          return {
+            product: { ...item.product, stock: product.stock - 1 },
+            quantity: item.quantity + 1,
+          };
+        }
+        return item;
+      })
+    );
+  };
 
-  const removeFromCart = (productId: string) => {};
+  const removeFromCart = (productId: string) => {
+    const removeItem = cart.filter((item) => item.product.id !== productId);
+    setCart(removeItem);
+  };
 
-  const updateQuantity = (productId: string, newQuantity: number) => {};
+  const updateQuantity = (productId: string, newQuantity: number) => {
+    const updatedCartItem = updateCartItemQuantity(
+      cart,
+      productId,
+      newQuantity
+    );
+    const targetProduct = cart.find((item) => item.product.id === productId);
+    if (targetProduct && targetProduct.product.stock === 0) {
+      return;
+    }
+
+    setCart(updatedCartItem);
+  };
 
   const applyCoupon = (coupon: Coupon) => {};
 
