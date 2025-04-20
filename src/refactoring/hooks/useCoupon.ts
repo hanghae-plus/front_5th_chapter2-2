cb9dc5e5f87264db32f3d6102a0bad2eaaeed20d
상은 <1-blue98@naver.com>
2025-04-20 12:21:44 +0900

    ✨: useCoupon 구현

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,14 @@
 import { useState } from "react";
-import { Coupon } from "../../types.ts";
+import type { Coupon } from "#src/types";
 
+// 엔티티를 다루는 훅
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  /** 쿠폰 추가 */
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+  };
+
+  return { coupons, addCoupon };
 };
