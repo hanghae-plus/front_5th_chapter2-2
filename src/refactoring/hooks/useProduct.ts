c73f5306406f705d3fae9955d5b991de307b857d
DohyungKim <ehgud456456@naver.com>
2025-04-20 18:18:24 +0900

    feat: 새로운 상품을 목록에 추가하는 addProduct 함수 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
   const [products, setProducts] = useState(initialProducts);
 
+  const addProduct = useCallback(
+    (product: Product) => {
+      setProducts((prev) => [...prev, product]);
+    },
+    [setProducts]
+  );
+
   return {
     products,
     updateProduct: () => undefined,
-    addProduct: () => undefined,
+    addProduct,
   };
 };
