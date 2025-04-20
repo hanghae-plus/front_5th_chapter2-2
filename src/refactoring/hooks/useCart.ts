61ddfa96617057cd066c938a029e298805eb68cc
DohyungKim <ehgud456456@naver.com>
2025-04-20 20:05:12 +0900

    feat: `calculateTotal` 함수 호출시 `calculateCartTotal` 함수 계값을 그대로 반환

--- src/refactoring/hooks/useCart.ts
+++ src/refactoring/hooks/useCart.ts
@@ -39,11 +39,7 @@ export const useCart = () => {
     setSelectedCoupon(coupon);
   };
 
-  const calculateTotal = () => ({
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
-  });
+  const calculateTotal = () => calculateCartTotal(cart, selectedCoupon);
 
   return {
     cart,
