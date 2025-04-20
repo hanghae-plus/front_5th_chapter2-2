0150f4f3afb9b9a10b98030b52696b2b2db095e3
anveloper <hitedin@gmail.com>
2025-04-20 22:32:48 +0900

    feat: 서비스 토글 및 뷰 조건문 수정

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
-import { Coupon, Product } from "../types.ts";
+import { Coupon, PERMISSION_TYPE, Product } from "../types.ts";
 import { AdminPage } from "./components/AdminPage.tsx";
 import { CartPage } from "./components/CartPage.tsx";
-import { useCoupons, useProducts } from "./hooks";
+import { useCoupons, useProducts, useService } from "./hooks";
 
 const initialProducts: Product[] = [
   {
@@ -46,23 +45,26 @@ const initialCoupons: Coupon[] = [
   },
 ];
 
+// 서버에서 사용자에게 관리자 권한이 있는 지를 더미 테스트
+const SERVER_USER_PERMISSION = PERMISSION_TYPE.ADMIN;
+
 const App = () => {
   const { products, updateProduct, addProduct } = useProducts(initialProducts);
   const { coupons, addCoupon } = useCoupons(initialCoupons);
-  const [isAdmin, setIsAdmin] = useState(false);
+  const { isAdminView, toggleService } = useService(SERVER_USER_PERMISSION);
 
   return (
     <div className="min-h-screen bg-gray-100">
       <nav className="bg-blue-600 text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
           <h1 className="text-2xl font-bold">쇼핑몰 관리 시스템</h1>
-          <button onClick={() => setIsAdmin(!isAdmin)} className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-blue-100">
-            {isAdmin ? "장바구니 페이지로" : "관리자 페이지로"}
+          <button onClick={toggleService} className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-blue-100">
+            {isAdminView ? "장바구니 페이지로" : "관리자 페이지로"}
           </button>
         </div>
       </nav>
       <main className="container mx-auto mt-6">
-        {isAdmin ? (
+        {isAdminView ? (
           <AdminPage products={products} coupons={coupons} onProductUpdate={updateProduct} onProductAdd={addProduct} onCouponAdd={addCoupon} />
         ) : (
           <CartPage products={products} coupons={coupons} />
