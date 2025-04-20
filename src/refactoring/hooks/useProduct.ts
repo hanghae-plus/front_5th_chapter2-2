128b4a281149af11fdc26875526914166282f783
hsw824 <ghdtjddn0824@naver.com>
2025-04-21 00:07:28 +0900

    feat: 관리자 페이지 상품 추가 로직 추가

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,25 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState(initialProducts);
+
+  const updateProduct = (updateProduct: Product) => {
+    setProducts(
+      products.map((product) => {
+        if (product.id === updateProduct.id) {
+          return updateProduct;
+        }
+        return product;
+      })
+    );
+  };
+
+  const addProduct = (addProduct: Product) => {
+    setProducts((prev) => [...prev, addProduct]);
+  };
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
