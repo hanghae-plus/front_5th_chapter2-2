21c414ae59e17ef410b20a6811c6de08d89e28b3
JYPPAP <yungsu22276@gmail.com>
2025-04-21 15:59:07 +0900

    1. `InitialInfo.tsx` 에 `initialProducts` , `initialCoupons` 데이터 분리 2. `useCoupons.ts` , `useProduct.ts` 에 data에서 직접 값을 가져와 사용하는 형태로 변경

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,16 @@
 import { useState } from "react";
 import { Coupon } from "../../types.ts";
+import {initialCoupons} from "../data/InitialInfo.tsx";
 
-export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+export const useCoupons = (initialData: Coupon[] = initialCoupons) => {
+  const [coupons, setCoupons] = useState<Coupon[]>(initialData);
+
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons(prevCoupons => [...prevCoupons, newCoupon]);
+  }
+
+  return {
+    coupons,
+    addCoupon,
+  }
 };
--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import { Product } from "../../types.ts";
+import {initialProducts} from "../data/InitialInfo.tsx";
 
-export const useProducts = (initialProducts: Product[]) => {
+export const useProducts = (initialData: Product[] = initialProducts) => {
+  const [products, setProducts] = useState<Product[]>(initialData);
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts(prevProducts =>
+      prevProducts.map(product =>
+        product.id === updatedProduct.id ? updatedProduct : product
+      )
+    );
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts(prevProducts => [...prevProducts, newProduct]);
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
