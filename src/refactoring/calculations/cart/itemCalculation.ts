45eab877e0208ec9c70630d5cf881db52751ac28
chloekim <chloe@perfitt.io>
2025-04-21 18:42:37 +0900

    chore(prettier): 설치 및 기본 포맷 설정 추가

--- src/refactoring/calculations/cart/itemCalculation.ts
+++ src/refactoring/calculations/cart/itemCalculation.ts
@@ -12,19 +12,20 @@ export const getRemainingStock = (product: Product, cart: CartItem[]): number =>
   return product.stock - (cartItem?.quantity || 0);
 };
 
-
 /**
  * 장바구니 아이템의 수량에 따라 적용 가능한 최대 할인율을 계산하는 순수 함수
  *
  * @param {CartItem} item - 장바구니 항목
- * @returns {number} - 적용된 할인율 
+ * @returns {number} - 적용된 할인율
  */
 
 export const getAppliedDiscount = (item: CartItem) => {
   const { discounts } = item.product;
   const { quantity } = item;
- 
-  const appliedDiscount = discounts.filter((discount) => quantity >= discount.quantity).reduce((max, discount) => Math.max(max, discount.rate), 0);
+
+  const appliedDiscount = discounts
+    .filter((discount) => quantity >= discount.quantity)
+    .reduce((max, discount) => Math.max(max, discount.rate), 0);
 
   return appliedDiscount;
 };
