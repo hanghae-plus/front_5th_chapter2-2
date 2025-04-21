71821a3b42ed58f5246c9c7eae29ea11126e2e3a
hanbeulYou <hanbeul.you@gmail.com>
2025-04-22 02:04:44 +0900

    refactor: PageLayout 컴포넌트 분리

--- src/refactoring/pages/CartPage.tsx
+++ src/refactoring/pages/CartPage.tsx
@@ -6,7 +6,7 @@ import {
   OrderSummary,
   CartItemCard,
 } from "../components/cart";
-import { Typography } from "../components/common";
+import { PageLayout, Typography } from "../components/common";
 
 interface Props {
   products: Product[];
@@ -49,57 +49,54 @@ export const CartPage = ({ products, coupons }: Props) => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <Typography variant="h1">장바구니</Typography>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div>
-          <Typography variant="h2">상품 목록</Typography>
-          <div className="space-y-2">
-            {products.map((product) => {
-              const remainingStock = getRemainingStock(product);
-              return (
-                <ProductCard
-                  key={product.id}
-                  product={product}
-                  remainingStock={remainingStock}
-                  onAddToCart={addToCart}
-                  getMaxDiscount={getMaxDiscount}
-                />
-              );
-            })}
-          </div>
-        </div>
-        <div>
-          <Typography variant="h2">장바구니 내역</Typography>
-
-          <div className="space-y-2">
-            {cart.map((item) => {
-              const appliedDiscount = getAppliedDiscount(item);
-              return (
-                <CartItemCard
-                  key={item.product.id}
-                  item={item}
-                  appliedDiscount={appliedDiscount}
-                  onUpdateQuantity={updateQuantity}
-                  onRemoveFromCart={removeFromCart}
-                />
-              );
-            })}
-          </div>
-
-          <CouponSection
-            coupons={coupons}
-            selectedCoupon={selectedCoupon}
-            onApplyCoupon={applyCoupon}
-          />
-
-          <OrderSummary
-            totalBeforeDiscount={totalBeforeDiscount}
-            totalDiscount={totalDiscount}
-            totalAfterDiscount={totalAfterDiscount}
-          />
+    <PageLayout title="장바구니">
+      <div>
+        <Typography variant="h2">상품 목록</Typography>
+        <div className="space-y-2">
+          {products.map((product) => {
+            const remainingStock = getRemainingStock(product);
+            return (
+              <ProductCard
+                key={product.id}
+                product={product}
+                remainingStock={remainingStock}
+                onAddToCart={addToCart}
+                getMaxDiscount={getMaxDiscount}
+              />
+            );
+          })}
         </div>
       </div>
-    </div>
+      <div>
+        <Typography variant="h2">장바구니 내역</Typography>
+
+        <div className="space-y-2">
+          {cart.map((item) => {
+            const appliedDiscount = getAppliedDiscount(item);
+            return (
+              <CartItemCard
+                key={item.product.id}
+                item={item}
+                appliedDiscount={appliedDiscount}
+                onUpdateQuantity={updateQuantity}
+                onRemoveFromCart={removeFromCart}
+              />
+            );
+          })}
+        </div>
+
+        <CouponSection
+          coupons={coupons}
+          selectedCoupon={selectedCoupon}
+          onApplyCoupon={applyCoupon}
+        />
+
+        <OrderSummary
+          totalBeforeDiscount={totalBeforeDiscount}
+          totalDiscount={totalDiscount}
+          totalAfterDiscount={totalAfterDiscount}
+        />
+      </div>
+    </PageLayout>
   );
 };
