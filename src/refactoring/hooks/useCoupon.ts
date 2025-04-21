12c5106b761ad3c5dd72896fc68aca579e66b0a6
YUJIN <yujin@YUJINui-MacBookAir.local>
2025-04-22 02:19:21 +0900

    fix: useCoupon, useProduct 테스트 통과

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,27 @@ import { useState } from "react";
 import { Coupon } from "../../types.ts";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+  };
+  const removeCoupon = (couponCode: string) => {
+    setCoupons((prevCoupons) =>
+      prevCoupons.filter((coupon) => coupon.code !== couponCode),
+    );
+  };
+  const applyCoupon = (couponCode: string) => {
+    const coupon = coupons.find((coupon) => coupon.code === couponCode);
+    // if (coupon) {
+
+    // }
+  };
+
+  return {
+    coupons,
+    addCoupon,
+    removeCoupon,
+    applyCoupon,
+  };
 };
