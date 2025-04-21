ce4007e914a0c22e1d50a92fd566bb70af69cfd0
naroso_o <naroso1751@naver.com>
2025-04-21 23:36:03 +0900

    feat: useProduct, useCoupon

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,29 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
+    const [products, setProducts] = useState(initialProducts);
+
+    const updateProduct = (product: Product) => {
+        // TODO: error 핸들링
+        if (!product) {
+            return;
+        }
+        
+        const newProducts = products.map((item) => {
+            return item.id === product.id ? product : item;
+        });
+
+        setProducts(newProducts);
+    };
+
+    const addProduct = (product: Product) => {
+        const newProducts = [...products, product];
+        setProducts(newProducts);
+    };
+
     return {
-        products: [],
-        updateProduct: () => undefined,
-        addProduct: () => undefined,
+        products,
+        updateProduct,
+        addProduct,
     };
 };
