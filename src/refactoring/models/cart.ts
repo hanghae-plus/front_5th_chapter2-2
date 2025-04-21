ad9132efd10dc4b9889012d7742e4a6a57f32e59
HoJae Lee <hojae.lee@softly.ai>
2025-04-22 02:04:41 +0900

    refactor: cart business logic

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,28 +1,102 @@
-import { CartItem, Coupon } from "../../types";
+import { CartItem, Coupon, Product } from "../../types";
+
+export const getRemainingStock = (cart: CartItem[], product: Product) => {
+	const cartItem = cart.find((item) => item.product.id === product.id);
+
+	const remainingStock = product.stock - (cartItem?.quantity || 0);
+	return remainingStock;
+};
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+	const {
+		quantity,
+		product: { price, discounts },
+	} = item;
+
+	const discount = discounts.reduce((maxDiscount, d) => {
+		return quantity >= d.quantity && d.rate > maxDiscount
+			? d.rate
+			: maxDiscount;
+	}, 0);
+
+	const itemTotalAfterDiscount = price * quantity * (1 - discount);
+	return itemTotalAfterDiscount;
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+	const {
+		quantity,
+		product: { discounts },
+	} = item;
+
+	const appliedDiscount = discounts.reduce((maxDiscount, discount) => {
+		if (quantity < discount.quantity) return maxDiscount;
+
+		return Math.max(maxDiscount, discount.rate);
+	}, 0);
+	return appliedDiscount;
 };
 
 export const calculateCartTotal = (
-  cart: CartItem[],
-  selectedCoupon: Coupon | null
+	cart: CartItem[],
+	selectedCoupon: Coupon | null
 ) => {
-  return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
-  };
+	let totalBeforeDiscount = 0;
+	let totalAfterDiscount = 0;
+
+	cart.forEach((item) => {
+		const {
+			quantity,
+			product: { price },
+		} = item;
+
+		const itemTotalAfterDiscount = calculateItemTotal(item);
+
+		totalBeforeDiscount += price * quantity;
+		totalAfterDiscount += itemTotalAfterDiscount;
+	});
+
+	let totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
+	// 쿠폰 적용
+	if (selectedCoupon) {
+		if (selectedCoupon.discountType === "amount") {
+			totalAfterDiscount = Math.max(
+				0,
+				totalAfterDiscount - selectedCoupon.discountValue
+			);
+		} else {
+			totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
+		}
+		totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+	}
+
+	return {
+		totalBeforeDiscount: Math.round(totalBeforeDiscount),
+		totalAfterDiscount: Math.round(totalAfterDiscount),
+		totalDiscount: Math.round(totalDiscount),
+	};
 };
 
 export const updateCartItemQuantity = (
-  cart: CartItem[],
-  productId: string,
-  newQuantity: number
+	cart: CartItem[],
+	productId: string,
+	newQuantity: number
 ): CartItem[] => {
-  return [];
+	const updatedCart = cart
+		.map((item) => {
+			const { id, stock } = item.product;
+
+			if (id !== productId) return item;
+
+			const maxQuantity = stock;
+			const updatedQuantity = Math.max(0, Math.min(newQuantity, maxQuantity));
+
+			return updatedQuantity > 0
+				? { ...item, quantity: updatedQuantity }
+				: null;
+		})
+		.filter((item): item is CartItem => item !== null);
+
+	return updatedCart;
 };
