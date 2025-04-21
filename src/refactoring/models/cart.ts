bc5fb72ffadbec7169d68bdc3e5d172470d2d618
hyunjin <71summernight@naver.com>
2025-04-21 21:23:08 +0900

    hook 함수 분리, utils 작업중 중간커밋

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,21 +1,30 @@
 import { CartItem, Coupon } from "../../types";
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  return item.product.price * item.quantity * (1 - getMaxApplicableDiscount(item));
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const applicableDiscounts = item.product.discounts.filter((discount) => discount.quantity <= item.quantity);
+  return applicableDiscounts.reduce((max, discount) => Math.max(max, discount.rate), 0);
 };
 
 export const calculateCartTotal = (
   cart: CartItem[],
   selectedCoupon: Coupon | null
 ) => {
+
+  console.log(cart)
+  console.log(selectedCoupon)
+  const totalBeforeDiscount = cart.reduce((sum, item) => sum + calculateItemTotal(item), 0);
+  const totalAfterDiscount = cart.reduce((sum, item) => sum + calculateItemTotal(item), 0);
+  const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
+  console.log('utils',totalBeforeDiscount, totalAfterDiscount, totalDiscount);
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
   };
 };
 
