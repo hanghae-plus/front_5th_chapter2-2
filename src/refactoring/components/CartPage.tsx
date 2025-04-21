4cb979016b7f8b6ecf0ef854d05a6f6f9ec47e26
dlsxody1 <dlsxody1@naver.com>
2025-04-21 21:09:32 +0900

    refactor : 주문요약 분리

--- src/refactoring/components/CartPage.tsx
+++ src/refactoring/components/CartPage.tsx
@@ -3,6 +3,7 @@ import { useCart } from "../hooks";
 import { calculateCartTotal } from "../models/cart.ts";
 import CartList from "./cart/CartList.tsx";
 import CouponSelector from "./cart/CouponSelector.tsx";
+import OrderSummary from "./cart/OrderSummary.tsx";
 import ProductList from "./cart/ProductList.tsx";
 
 interface Props {
@@ -36,25 +37,16 @@ export const CartPage = ({ products, coupons }: Props) => {
             removeFromCart={removeFromCart}
             updateQuantity={updateQuantity}
           />
-
           <CouponSelector
             coupons={coupons}
             selectedCoupon={selectedCoupon}
             applyCoupon={applyCoupon}
           />
-
-          <div className="mt-6 bg-white p-4 rounded shadow">
-            <h2 className="text-2xl font-semibold mb-2">주문 요약</h2>
-            <div className="space-y-1">
-              <p>상품 금액: {totalBeforeDiscount.toLocaleString()}원</p>
-              <p className="text-green-600">
-                할인 금액: {totalDiscount.toLocaleString()}원
-              </p>
-              <p className="text-xl font-bold">
-                최종 결제 금액: {totalAfterDiscount.toLocaleString()}원
-              </p>
-            </div>
-          </div>
+          <OrderSummary
+            totalBeforeDiscount={totalBeforeDiscount}
+            totalDiscount={totalDiscount}
+            totalAfterDiscount={totalAfterDiscount}
+          />
         </div>
       </div>
     </div>
