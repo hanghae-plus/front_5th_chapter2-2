d9107afbfeb616975c297752c5da685897bf6dba
dlsxody1 <dlsxody1@naver.com>
2025-04-21 21:07:47 +0900

    refactor : 쿠폰 선택 분리

--- src/refactoring/components/CartPage.tsx
+++ src/refactoring/components/CartPage.tsx
@@ -2,6 +2,7 @@ import { Coupon, Product } from "../../types.ts";
 import { useCart } from "../hooks";
 import { calculateCartTotal } from "../models/cart.ts";
 import CartList from "./cart/CartList.tsx";
+import CouponSelector from "./cart/CouponSelector.tsx";
 import ProductList from "./cart/ProductList.tsx";
 
 interface Props {
@@ -36,32 +37,11 @@ export const CartPage = ({ products, coupons }: Props) => {
             updateQuantity={updateQuantity}
           />
 
-          <div className="mt-6 bg-white p-4 rounded shadow">
-            <h2 className="text-2xl font-semibold mb-2">쿠폰 적용</h2>
-            <select
-              onChange={(e) => applyCoupon(coupons[parseInt(e.target.value)])}
-              className="w-full p-2 border rounded mb-2"
-            >
-              <option value="">쿠폰 선택</option>
-              {coupons.map((coupon, index) => (
-                <option key={coupon.code} value={index}>
-                  {coupon.name} -{" "}
-                  {coupon.discountType === "amount"
-                    ? `${coupon.discountValue}원`
-                    : `${coupon.discountValue}%`}
-                </option>
-              ))}
-            </select>
-            {selectedCoupon && (
-              <p className="text-green-600">
-                적용된 쿠폰: {selectedCoupon.name}(
-                {selectedCoupon.discountType === "amount"
-                  ? `${selectedCoupon.discountValue}원`
-                  : `${selectedCoupon.discountValue}%`}{" "}
-                할인)
-              </p>
-            )}
-          </div>
+          <CouponSelector
+            coupons={coupons}
+            selectedCoupon={selectedCoupon}
+            applyCoupon={applyCoupon}
+          />
 
           <div className="mt-6 bg-white p-4 rounded shadow">
             <h2 className="text-2xl font-semibold mb-2">주문 요약</h2>
