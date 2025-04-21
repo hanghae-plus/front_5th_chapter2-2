b2a189ce8e087cbb091058f3297b3d2f2356230c
hanbeulYou <hanbeul.you@gmail.com>
2025-04-21 12:29:32 +0900

    feat: useCart 훅의 카트 수량 관련 로직 추가

--- src/refactoring/hooks/useCart.ts
+++ src/refactoring/hooks/useCart.ts
@@ -7,11 +7,25 @@ export const useCart = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
 
-  const addToCart = (product: Product) => {};
+  const addToCart = (product: Product) => {
+    setCart((prevCart) => [...prevCart, { product, quantity: 1 }]);
+  };
 
-  const removeFromCart = (productId: string) => {};
+  const removeFromCart = (productId: string) => {
+    setCart((prevCart) =>
+      prevCart.filter((item) => item.product.id !== productId)
+    );
+  };
 
-  const updateQuantity = (productId: string, newQuantity: number) => {};
+  const updateQuantity = (productId: string, newQuantity: number) => {
+    setCart((prevCart) =>
+      prevCart.map((item) =>
+        item.product.id === productId
+          ? { ...item, quantity: newQuantity }
+          : item
+      )
+    );
+  };
 
   const applyCoupon = (coupon: Coupon) => {};
 
