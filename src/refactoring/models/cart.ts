3b0fead11204dd5506bfc6858f9fcc307e194f55
hsw824 <ghdtjddn0824@naver.com>
2025-04-21 22:55:48 +0900

    refactor: cart 데이터를 다루는 함수 추가

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,21 +1,69 @@
 import { CartItem, Coupon } from "../../types";
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  let totalBeforeDiscount = 0;
+  let totalAfterDiscount = 0;
+
+  const { price } = item.product;
+  const { quantity } = item;
+  totalBeforeDiscount += price * quantity;
+  const discount = getMaxApplicableDiscount(item);
+
+  totalAfterDiscount += price * quantity * (1 - discount);
+
+  return totalAfterDiscount;
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const { quantity } = item;
+  const discount = item.product.discounts.reduce((maxDiscount, d) => {
+    return quantity >= d.quantity && d.rate > maxDiscount
+      ? d.rate
+      : maxDiscount;
+  }, 0);
+  return discount;
 };
 
 export const calculateCartTotal = (
   cart: CartItem[],
   selectedCoupon: Coupon | null
 ) => {
+  let totalBeforeDiscount = 0;
+  let totalAfterDiscount = 0;
+
+  cart.forEach((item) => {
+    const { price } = item.product;
+    const { quantity } = item;
+    totalBeforeDiscount += price * quantity;
+
+    const discount = item.product.discounts.reduce((maxDiscount, d) => {
+      return quantity >= d.quantity && d.rate > maxDiscount
+        ? d.rate
+        : maxDiscount;
+    }, 0);
+
+    totalAfterDiscount += price * quantity * (1 - discount);
+  });
+
+  let totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
+  // 쿠폰 적용
+  if (selectedCoupon) {
+    if (selectedCoupon.discountType === "amount") {
+      totalAfterDiscount = Math.max(
+        0,
+        totalAfterDiscount - selectedCoupon.discountValue
+      );
+    } else {
+      totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
+    }
+    totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+  }
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
   };
 };
 
@@ -24,19 +72,18 @@ export const updateCartItemQuantity = (
   productId: string,
   newQuantity: number
 ): CartItem[] => {
-  if (newQuantity === 0) {
-    return cart.filter((item) => item.product.id !== productId);
-  }
+  const updatedCartItem = cart
+    .map((item) => {
+      if (item.product.id === productId) {
+        const maxQuantity = item.product.stock;
+        const updatedQuantity = Math.max(0, Math.min(newQuantity, maxQuantity));
+        return updatedQuantity > 0
+          ? { ...item, quantity: updatedQuantity }
+          : null;
+      }
+      return item;
+    })
+    .filter((item): item is CartItem => item !== null);
 
-  const computedCart = cart.map((item) => {
-    if (item.product.id === productId) {
-      return {
-        product: { ...item.product, stock: item.product.stock - 1 },
-        quantity: newQuantity,
-      };
-    }
-    return item;
-  });
-
-  return computedCart;
+  return updatedCartItem;
 };
