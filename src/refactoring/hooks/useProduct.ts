eff1637e5642d9e9e4f046b4f570cb3637fb89a4
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-21 16:00:35 +0900

    Feat: useProducts

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,33 @@
 import { useState } from "react";
 import { Product } from "../../types.ts";
 
+/**
+ *액션: updateProduct, addProduct
+ *계산:
+ *데이터: initialProducts, products -> state와 props
+ */
 export const useProducts = (initialProducts: Product[]) => {
+  //  TODO: 액션에서 계산 분리하기
+
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  /**상품을 업데이트합니다. */
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) =>
+        p.id === updatedProduct.id ? updatedProduct : p,
+      ),
+    );
+  };
+
+  /**상품을 추가합니다. */
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
