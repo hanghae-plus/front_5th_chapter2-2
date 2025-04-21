de0d5c546b92557440e8138b063be0e1d580ceb3
naroso_o <naroso1751@naver.com>
2025-04-21 22:35:30 +0900

    chore: apply prettierrc

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { Coupon } from "../../types.ts";
+import { useState } from 'react';
+import { Coupon } from '../../types.ts';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+    return { coupons: [], addCoupon: () => undefined };
 };
