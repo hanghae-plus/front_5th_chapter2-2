6a11ccb69daaabe5ec271051c9498a7457cc7f3b
JiHwanLee <kdgosu53@gmail.com>
2025-04-22 11:34:59 +0900

    useCoupon 개발

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,11 @@ import { useState } from "react";
 import { Coupon } from "../../types.ts";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+    const [coupons, setCoupons] = useState(initialCoupons);
+
+    // 쿠폰 추가 함수
+    const addCoupon = (newCoupon: Coupon) => {
+        setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+    };
+    return { coupons, addCoupon };
 };
