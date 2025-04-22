9dd9e021155a90460900043ab093db569116e17e
Doh Kim <ehgud456456@naver.com>
2025-04-22 14:18:01 +0900

    refactor: initial data를 shared/mock 폴더로 분리

--- src/refactoring/app/App.tsx
+++ src/refactoring/app/App.tsx
@@ -1,53 +1,12 @@
 import { useState } from "react";
 
-import { Coupon, Product } from "@/types.ts";
-
 import { AdminPage, CartPage } from "@r/components";
 
 import { useProducts } from "@r/hooks/useProduct.ts";
 import { useCoupons } from "@r/hooks/useCoupon.ts";
 
-const initialProducts: Product[] = [
-  {
-    id: "p1",
-    name: "상품1",
-    price: 10000,
-    stock: 20,
-    discounts: [
-      { quantity: 10, rate: 0.1 },
-      { quantity: 20, rate: 0.2 },
-    ],
-  },
-  {
-    id: "p2",
-    name: "상품2",
-    price: 20000,
-    stock: 20,
-    discounts: [{ quantity: 10, rate: 0.15 }],
-  },
-  {
-    id: "p3",
-    name: "상품3",
-    price: 30000,
-    stock: 20,
-    discounts: [{ quantity: 10, rate: 0.2 }],
-  },
-];
-
-const initialCoupons: Coupon[] = [
-  {
-    name: "5000원 할인 쿠폰",
-    code: "AMOUNT5000",
-    discountType: "amount",
-    discountValue: 5000,
-  },
-  {
-    name: "10% 할인 쿠폰",
-    code: "PERCENT10",
-    discountType: "percentage",
-    discountValue: 10,
-  },
-];
+import { initialCoupons } from "@r/shared/mock/initial-coupons";
+import { initialProducts } from "@r/shared/mock/initial-products";
 
 const App = () => {
   const { products, updateProduct, addProduct } = useProducts(initialProducts);
