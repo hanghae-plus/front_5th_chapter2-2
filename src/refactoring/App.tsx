9a1dbe9da09fbad3c29cf4a4bbc53f575527442a
HoJae Lee <hojae.lee@softly.ai>
2025-04-23 04:50:13 +0900

    refactor: separate mock data into mock folder

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,61 +1,21 @@
 import { useState } from "react";
 import { CartPage } from "./pages/CartPage.tsx";
 import { AdminPage } from "./pages/AdminPage.tsx";
-import { Coupon, Product } from "../types.ts";
-import { useCoupons, useProducts } from "./hooks";
 import { Button } from "./components/ui/Button.tsx";
-
-const initialProducts: Product[] = [
-	{
-		id: "p1",
-		name: "상품1",
-		price: 10000,
-		stock: 20,
-		discounts: [
-			{ quantity: 10, rate: 0.1 },
-			{ quantity: 20, rate: 0.2 },
-		],
-	},
-	{
-		id: "p2",
-		name: "상품2",
-		price: 20000,
-		stock: 20,
-		discounts: [{ quantity: 10, rate: 0.15 }],
-	},
-	{
-		id: "p3",
-		name: "상품3",
-		price: 30000,
-		stock: 20,
-		discounts: [{ quantity: 10, rate: 0.2 }],
-	},
-];
-
-const initialCoupons: Coupon[] = [
-	{
-		name: "5000원 할인 쿠폰",
-		code: "AMOUNT5000",
-		discountType: "amount",
-		discountValue: 5000,
-	},
-	{
-		name: "10% 할인 쿠폰",
-		code: "PERCENT10",
-		discountType: "percentage",
-		discountValue: 10,
-	},
-];
+import { MOCK_PRODUCTS } from "./__mocks__/products.ts";
+import { MOCK_COUPONS } from "./__mocks__/coupons.ts";
+import { useProducts } from "./hooks/useProduct.ts";
+import { useCoupons } from "./hooks/useCoupon.ts";
 
 const App = () => {
-	const { products, updateProduct, addProduct } = useProducts(initialProducts);
-	const { coupons, addCoupon } = useCoupons(initialCoupons);
+	const { products, updateProduct, addProduct } = useProducts(MOCK_PRODUCTS);
+	const { coupons, addCoupon } = useCoupons(MOCK_COUPONS);
 	const [isAdmin, setIsAdmin] = useState(false);
 
 	return (
 		<div className="min-h-screen bg-gray-100">
-			<nav className="bg-blue-600 text-white p-4">
-				<div className="container mx-auto flex justify-between items-center">
+			<nav className="bg-blue-600 p-4 text-white">
+				<div className="container mx-auto flex items-center justify-between">
 					<h1 className="text-2xl font-bold">쇼핑몰 관리 시스템</h1>
 					<Button color="white" onClick={() => setIsAdmin(!isAdmin)}>
 						{isAdmin ? "장바구니 페이지로" : "관리자 페이지로"}
