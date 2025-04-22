ba36396de707b2e279a301f07e6c8e066ef806b7
JiYu <eunjun1018@gmail.com>
2025-04-22 13:17:00 +0900

    상품목록 useProducts 적용

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,21 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
-  return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  // 제품 업데이트 함수
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts(prevProducts => 
+      prevProducts.map(product => 
+        product.id === updatedProduct.id ? updatedProduct : product
+      )
+    );
   };
+
+   // 제품 추가 함수
+   const addProduct = (newProduct: Product) => {
+    setProducts(prevProducts => [...prevProducts, newProduct]);
+  };
+
+  return { products, updateProduct, addProduct };
 };
