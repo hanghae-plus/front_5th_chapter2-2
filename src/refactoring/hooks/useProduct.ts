6274db7fee954399fd26d0e37d86570a29434c4d
minhee <jmh3699@naver.com>
2025-04-22 12:35:30 +0900

    feat: useProducts 훅의 상품 상태 관리 기능 구현 - 초기값 할당, 상품 업데이트, 상품 추가 기능 구현함

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,8 +1,23 @@
 import { useState } from 'react';
 import { Product } from '../../types.ts';
 
-export const useProducts = (initialProducts: Product[]) => ({
-  products: [],
-  updateProduct: () => undefined,
-  addProduct: () => undefined,
-});
+// 상품 목록 관리 커스텀 훅
+export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (product: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) => (p.id === product.id ? product : p)),
+    );
+  };
+
+  const addProduct = (product: Product) => {
+    setProducts((prevProducts) => [...prevProducts, product]);
+  };
+
+  return {
+    products,
+    updateProduct,
+    addProduct,
+  };
+};
