2b9da9453f8555cf2942c34527538cc4d17063d3
anveloper <hitedin@gmail.com>
2025-04-22 23:19:49 +0900

    refactor: 기본(더미?) 데이터 배열을 각 Provider에서 미할당 시 각각 사용

--- src/refactoring/provider/CouponProvider.tsx
+++ src/refactoring/provider/CouponProvider.tsx
@@ -5,8 +5,8 @@ import { createContext, ReactNode, useContext } from "react";
 type CouponContextType = ReturnType<typeof useCoupons>;
 const CouponContext = createContext<CouponContextType | null>(null);
 
-export const CouponProvider = ({ initialCoupons, children }: { initialCoupons: Coupon[]; children: ReactNode }) => {
-  const value = useCoupons(initialCoupons);
+export const CouponProvider = ({ coupons = initialCoupons, children }: { coupons?: Coupon[]; children: ReactNode }) => {
+  const value = useCoupons(coupons);
   return <CouponContext.Provider value={value}>{children}</CouponContext.Provider>;
 };
 
@@ -15,3 +15,18 @@ export const useCouponContext = () => {
   if (!context) throw new Error("context is null");
   return context;
 };
+
+const initialCoupons: Coupon[] = [
+  {
+    name: "5000원 할인 쿠폰",
+    code: "AMOUNT5000",
+    discountType: "amount",
+    discountValue: 5000,
+  },
+  {
+    name: "10% 할인 쿠폰",
+    code: "PERCENT10",
+    discountType: "percentage",
+    discountValue: 10,
+  },
+];
--- src/refactoring/provider/index.tsx
+++ src/refactoring/provider/index.tsx
@@ -5,54 +5,12 @@ import { ProductProvider } from "./ProductProvider";
 export { useCouponContext } from "./CouponProvider";
 export { useProductContext } from "./ProductProvider";
 
-const initialProducts: Product[] = [
-  {
-    id: "p1",
-    name: "상품1",
-    price: 10000,
-    stock: 20,
-    discounts: [
-      { quantity: 10, rate: 0.1 },
-      { quantity: 20, rate: 0.2 },
-    ],
-  },
-  {
-    id: "p2",
-    name: "상품2",
-    price: 20000,
-    stock: 20,
-    discounts: [{ quantity: 10, rate: 0.15 }],
-  },
-  {
-    id: "p3",
-    name: "상품3",
-    price: 30000,
-    stock: 20,
-    discounts: [{ quantity: 10, rate: 0.2 }],
-  },
-];
-
-const initialCoupons: Coupon[] = [
-  {
-    name: "5000원 할인 쿠폰",
-    code: "AMOUNT5000",
-    discountType: "amount",
-    discountValue: 5000,
-  },
-  {
-    name: "10% 할인 쿠폰",
-    code: "PERCENT10",
-    discountType: "percentage",
-    discountValue: 10,
-  },
-];
-
 type Props = { products?: Product[]; coupons?: Coupon[]; children: ReactNode };
 
-export const Providers = ({ products = initialProducts, coupons = initialCoupons, children }: Props) => {
+export const Providers = ({ products, coupons, children }: Props) => {
   return (
-    <ProductProvider initialProducts={products}>
-      <CouponProvider initialCoupons={coupons}>{children}</CouponProvider>
+    <ProductProvider products={products}>
+      <CouponProvider coupons={coupons}>{children}</CouponProvider>
     </ProductProvider>
   );
 };
