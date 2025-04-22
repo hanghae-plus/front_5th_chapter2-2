901dcbef0365fec1ad8b4c11a85bfe8d4d3d3873
devchaeyoung <chaemaa00@gmail.com>
2025-04-22 23:24:59 +0900

    feat: useProducts 추가

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,28 @@
-import { useState } from "react";
-import { Product } from "../../types.ts";
+import { useState } from 'react';
+import { Product } from '../../types.ts';
+
+const updateProductInList = (products: Product[], updatedProduct: Product) => {
+  return products.map((product) => (product.id === updatedProduct.id ? updatedProduct : product));
+};
+
+const addProductToList = (products: Product[], newProduct: Product) => {
+  return [...products, newProduct];
+};
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts(updateProductInList(products, updatedProduct));
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts(addProductToList(products, newProduct));
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
