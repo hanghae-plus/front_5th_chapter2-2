f3548fb61f0eff2044830377df0872a9c9154e11
3jun <kk3june@gmail.com>
2025-04-24 01:52:42 +0900

    refactor. 관리자 페이지 컴포넌트 분리

---
+++ src/refactoring/components/admin/ProductEdit.tsx
@@ -0,0 +1,44 @@
+import { Product } from '../../../types'
+
+interface Props {
+	editingProduct: Product
+	handleProductNameUpdate: (productId: string, newName: string) => void
+	handlePriceUpdate: (productId: string, newPrice: number) => void
+	handleStockUpdate: (productId: string, newStock: number) => void
+}	
+
+function ProductEdit({ editingProduct, handleProductNameUpdate, handlePriceUpdate, handleStockUpdate }: Props) {
+	return (
+		<div>
+			<div className="mb-4">
+				<label className="block mb-1">상품명: </label>
+				<input
+					type="text"
+					value={editingProduct.name}
+					onChange={(e) => handleProductNameUpdate(editingProduct.id, e.target.value)}
+					className="w-full p-2 border rounded"
+				/>
+			</div>
+			<div className="mb-4">
+				<label className="block mb-1">가격: </label>
+				<input
+					type="number"
+					value={editingProduct.price}
+					onChange={(e) => handlePriceUpdate(editingProduct.id, parseInt(e.target.value))}
+					className="w-full p-2 border rounded"
+				/>
+			</div>
+			<div className="mb-4">
+				<label className="block mb-1">재고: </label>
+				<input
+					type="number"
+					value={editingProduct.stock}
+					onChange={(e) => handleStockUpdate(editingProduct.id, parseInt(e.target.value))}
+					className="w-full p-2 border rounded"
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default ProductEdit
---
+++ src/refactoring/components/admin/ProductToggleListItem.tsx
@@ -0,0 +1,20 @@
+import { Product } from '../../../types'
+
+interface Props {
+	product: Product
+	toggleProductAccordion: (productId: string) => void
+}
+
+function ProductToggleListItem({ product, toggleProductAccordion }: Props) {
+	return (
+		<button
+		data-testid="toggle-button"
+		onClick={() => toggleProductAccordion(product.id)}
+		className="w-full text-left font-semibold"
+		>
+			{product.name} - {product.price}원 (재고: {product.stock})
+		</button>
+	)
+}
+
+export default ProductToggleListItem
