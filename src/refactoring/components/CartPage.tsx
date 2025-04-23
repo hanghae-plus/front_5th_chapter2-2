1cb6eb5edbcfae3489eae75d19874d3ac8ed26ad
hyunjin <71summernight@naver.com>
2025-04-23 22:45:20 +0900

    hook 합치기

--- src/refactoring/components/CartPage.tsx
+++ src/refactoring/components/CartPage.tsx
@@ -1,7 +1,7 @@
-import { CartItem, Coupon, Product } from '../../types.ts';
+import { Coupon, Product } from '../../types.ts';
 import { useCart } from "../hooks";
 
-interface Props {
+type Props = {
   products: Product[];
   coupons: Coupon[];
 }
@@ -14,31 +14,15 @@ export const CartPage = ({ products, coupons }: Props) => {
     updateQuantity,
     applyCoupon,
     calculateTotal,
+    getRemainingStock,
+    getMaxDiscount,
+    getAppliedDiscount,
     selectedCoupon
   } = useCart();
 
-  const getMaxDiscount = (discounts: { quantity: number; rate: number }[]) => {
-    return discounts.reduce((max, discount) => Math.max(max, discount.rate), 0);
-  };
-
-  const getRemainingStock = (product: Product) => {
-    const cartItem = cart.find(item => item.product.id === product.id);
-    return product.stock - (cartItem?.quantity || 0);
-  };
 
   const { totalBeforeDiscount, totalAfterDiscount, totalDiscount } = calculateTotal()
 
-  const getAppliedDiscount = (item: CartItem) => {
-    const { discounts } = item.product;
-    const { quantity } = item;
-    let appliedDiscount = 0;
-    for (const discount of discounts) {
-      if (quantity >= discount.quantity) {
-        appliedDiscount = Math.max(appliedDiscount, discount.rate);
-      }
-    }
-    return appliedDiscount;
-  };
 
   return (
     <div className="container mx-auto p-4">
--- src/refactoring/hooks/useCart.ts
+++ src/refactoring/hooks/useCart.ts
@@ -81,6 +81,9 @@ export const useCart = () => {
     updateQuantity,
     applyCoupon,
     calculateTotal,
+    getAppliedDiscount,
+    getRemainingStock,
+    getMaxDiscount,
     selectedCoupon,
   };
 };
