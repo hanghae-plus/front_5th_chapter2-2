6ec7b675b3cd9dfcd0eb66833c0e206f6cc354d9
hanbeulYou <hanbeul.you@gmail.com>
2025-04-23 18:29:42 +0900

    refactor: Admin 페이지에서 사용되는 Product 관련 로직 useProductManagement 훅으로 분리

---
+++ src/refactoring/hooks/product/useProductCreate.ts
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Product } from "../../../types";
+
+export const useProductCreate = ({
+  onProductAdd,
+}: {
+  onProductAdd: (product: Product) => void;
+}) => {
+  const [showNewProductForm, setShowNewProductForm] = useState(false);
+  const [newProduct, setNewProduct] = useState<Omit<Product, "id">>({
+    name: "",
+    price: 0,
+    stock: 0,
+    discounts: [],
+  });
+
+  const handleAddNewProduct = () => {
+    const productWithId = {
+      ...newProduct,
+      id: Date.now().toString(),
+    };
+    onProductAdd(productWithId);
+    setNewProduct({
+      name: "",
+      price: 0,
+      stock: 0,
+      discounts: [],
+    });
+    setShowNewProductForm(false);
+  };
+
+  return {
+    showNewProductForm,
+    setShowNewProductForm,
+    newProduct,
+    setNewProduct,
+    handleAddNewProduct,
+  };
+};
---
+++ src/refactoring/hooks/product/useProductManagement.ts
@@ -0,0 +1,60 @@
+// hooks/product/useProductManagement.ts
+import { Product } from "../../../types";
+import { useAccordion } from "../common/useAccordion";
+import { useProductEdit } from "./useProductEdit";
+import { useProductDiscount } from "./useProductDiscount";
+import { useProductCreate } from "./useProductCreate";
+
+interface UseProductManagementProps {
+  onProductUpdate: (updatedProduct: Product) => void;
+  onProductAdd: (newProduct: Product) => void;
+}
+
+export const useProductManagement = ({
+  onProductUpdate,
+  onProductAdd,
+}: UseProductManagementProps) => {
+  const accordion = useAccordion<string>();
+  const productEdit = useProductEdit({ onProductUpdate });
+  const discount = useProductDiscount();
+  const productCreate = useProductCreate({ onProductAdd });
+
+  const handleDiscountAdd = (productId: string) => {
+    if (productEdit.editingProduct?.id === productId) {
+      const updatedProduct = discount.handleAddDiscount(
+        productEdit.editingProduct
+      );
+      productEdit.handleFieldUpdate(
+        productId,
+        "discounts",
+        updatedProduct.discounts
+      );
+      discount.setNewDiscount({ quantity: 0, rate: 0 });
+    }
+  };
+
+  const handleDiscountRemove = (productId: string, index: number) => {
+    if (productEdit.editingProduct?.id === productId) {
+      const updatedProduct = discount.handleRemoveDiscount(
+        productEdit.editingProduct,
+        index
+      );
+      productEdit.handleFieldUpdate(
+        productId,
+        "discounts",
+        updatedProduct.discounts
+      );
+    }
+  };
+
+  return {
+    accordion,
+    productEdit,
+    discount: {
+      ...discount,
+      handleDiscountAdd,
+      handleDiscountRemove,
+    },
+    productCreate,
+  };
+};
