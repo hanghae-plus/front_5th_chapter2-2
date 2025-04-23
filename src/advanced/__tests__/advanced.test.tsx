80f177b1ded4004130757406a121232d54f0aa2d
maphnew <zcm3118@gmail.com>
2025-04-23 23:32:14 +0900

    🩹 fix: advanced test code 수정

--- src/advanced/__tests__/advanced.test.tsx
+++ src/advanced/__tests__/advanced.test.tsx
@@ -17,6 +17,7 @@ import {
   removeLocalStorage,
   useLocalStorage,
 } from "../../refactoring/hooks";
+import { ProductsProvider } from "../../refactoring/providers";
 
 const mockProducts: Product[] = [
   {
@@ -57,38 +58,33 @@ const mockCoupons: Coupon[] = [
 ];
 
 const TestAdminPage = () => {
-  const [products, setProducts] = useState<Product[]>(mockProducts);
   const [coupons, setCoupons] = useState<Coupon[]>(mockCoupons);
-
-  const handleProductUpdate = (updatedProduct: Product) => {
-    setProducts((prevProducts) =>
-      prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p))
-    );
-  };
-
-  const handleProductAdd = (newProduct: Product) => {
-    setProducts((prevProducts) => [...prevProducts, newProduct]);
-  };
 
   const handleCouponAdd = (newCoupon: Coupon) => {
     setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
   };
 
   return (
-    <AdminPage
-      products={products}
-      coupons={coupons}
-      onProductUpdate={handleProductUpdate}
-      onProductAdd={handleProductAdd}
-      onCouponAdd={handleCouponAdd}
-    />
+    <ProductsProvider initialProducts={mockProducts}>
+      <AdminPage coupons={coupons} onCouponAdd={handleCouponAdd} />
+    </ProductsProvider>
+  );
+};
+
+const TestCartPage = () => {
+  const [coupons] = useState<Coupon[]>(mockCoupons);
+
+  return (
+    <ProductsProvider initialProducts={mockProducts}>
+      <CartPage coupons={coupons} />
+    </ProductsProvider>
   );
 };
 
 describe("advanced > ", () => {
   describe("시나리오 테스트 > ", () => {
     test("장바구니 페이지 테스트 > ", async () => {
-      render(<CartPage products={mockProducts} coupons={mockCoupons} />);
+      render(<TestCartPage />);
       const product1 = screen.getByTestId("product-p1");
       const product2 = screen.getByTestId("product-p2");
       const product3 = screen.getByTestId("product-p3");
