86eea9935c1f77957dbc4fabd63a65ea5bb56d69
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-23 22:44:22 +0900

    Refactor: Layout/Nav 생성

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -2,7 +2,7 @@ import { CartPage } from "./pages/CartPage.tsx";
 import { AdminPage } from "./pages/AdminPage.tsx";
 import { useAdmin, useCoupons, useProducts } from "./hooks";
 import { initialProducts, initialCoupons } from "./constants";
-
+import Nav from "./layout/Nav.tsx";
 const App = () => {
   //액션, 계산, 데이터
   const { isAdmin, toggleIsAdmin } = useAdmin();
@@ -13,17 +13,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <nav className="bg-blue-600 text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">쇼핑몰 관리 시스템</h1>
-          <button
-            onClick={toggleIsAdmin}
-            className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-blue-100"
-          >
-            {isAdmin ? "장바구니 페이지로" : "관리자 페이지로"}
-          </button>
-        </div>
-      </nav>
+      <Nav isAdmin={isAdmin} toggleIsAdmin={toggleIsAdmin} />
       <main className="container mx-auto mt-6">
         {isAdmin ? (
           <AdminPage
--- src/refactoring/hooks/useAdmin.ts
+++ src/refactoring/hooks/useAdmin.ts
@@ -1,9 +1,11 @@
 import { useState } from "react";
-
+//엔티티를 다루지 않는 훅
 export const useAdmin = () => {
   const [isAdmin, setIsAdmin] = useState(false);
+
   const toggleIsAdmin = () => {
     setIsAdmin(!isAdmin);
   };
+
   return { isAdmin, toggleIsAdmin };
 };
