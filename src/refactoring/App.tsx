7daf4a001cd910c1762ca11cd3720e8b6e9cce10
KumJungMin <rmawjdals@naver.com>
2025-04-23 23:31:17 +0900

    refactor: Main 영역을 별도 컴포넌트로 분리하기

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,27 +1,13 @@
-import { useState } from 'react';
-import { CartPage } from './components/CartPage.tsx';
-import { AdminPage } from './components/AdminPage.tsx';
-import { Coupon } from '../types.ts';
-import { useCoupons } from './hooks';
-import NavigationHeader from './components/NavigationHeader.tsx';
 import { ProductProvider } from './contexts/productContext.tsx';
 import { CouponProvider } from './contexts/couponContext.tsx';
 
-const App = () => {
-  const [isAdmin, setIsAdmin] = useState(false);
+import Main from './components/Main.tsx';
 
+const App = () => {
   return (
     <ProductProvider>
       <CouponProvider>
-        <div className="min-h-screen bg-gray-100">
-          <NavigationHeader
-            isAdmin={isAdmin}
-            onSwitchPage={() => setIsAdmin(!isAdmin)}
-          />
-          <main className="container mx-auto mt-6">
-            {isAdmin ? <AdminPage /> : <CartPage />}
-          </main>
-        </div>
+        <Main />
       </CouponProvider>
     </ProductProvider>
   );
---
+++ src/refactoring/components/Main.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+import NavigationHeader from './NavigationHeader.tsx';
+import { CartPage } from './CartPage.tsx';
+import { AdminPage } from './AdminPage.tsx';
+
+const Main = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <NavigationHeader
+        isAdmin={isAdmin}
+        onSwitchPage={() => setIsAdmin(!isAdmin)}
+      />
+      <main className="container mx-auto mt-6">
+        {isAdmin ? <AdminPage /> : <CartPage />}
+      </main>
+    </div>
+  );
+};
+
+export default Main;
