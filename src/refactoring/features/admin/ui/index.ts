3997602d3b19a2f0037c9f15372ee40ae5defb86
zlrlo <easy10612@gmail.com>
2025-04-24 00:11:51 +0900

    refactor: features admin 분리

---
+++ src/refactoring/features/admin/ui/index.ts
@@ -0,0 +1,4 @@
+export { NewProductForm } from './NewProductForm';
+export { ProductList } from './ProductList';
+export { CouponList } from './CouponList';
+export { NewCouponForm } from './NewCouponForm';
