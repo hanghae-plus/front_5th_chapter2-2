75bd18eb1550bd99d6068ae1bd390296fa4ce42c
zlrlo <easy10612@gmail.com>
2025-04-23 23:54:56 +0900

    refactor: widgets 분리
    - CouponManager, ProductManager

--- src/refactoring/widgets/index.ts
+++ src/refactoring/widgets/index.ts
@@ -1,2 +1,4 @@
 export { ProductSection } from './ProductSection';
 export { CartSection } from './CartSection';
+export { ProductManager } from './ProductManager';
+export { CouponManager } from './CouponManager';
