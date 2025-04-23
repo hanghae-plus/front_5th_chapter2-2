3049e3bbfb78124247d9c3d4baa74fb33719322d
KumJungMin <rmawjdals@naver.com>
2025-04-23 19:40:53 +0900

    refactor: useProduct 구현하기

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,32 @@
-import { useState } from "react";
-import { Product } from "../../types.ts";
+import { useState } from 'react';
+import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  function updateProduct(product: Partial<Product>) {
+    setProducts((prevProducts) => {
+      const targetIndex = prevProducts.findIndex((p) => p.id === product.id);
+      if (targetIndex === -1) {
+        throw new Error(`Product with id ${product.id} not found`);
+      } else {
+        const updatedProducts = [...prevProducts];
+        updatedProducts[targetIndex] = {
+          ...updatedProducts[targetIndex],
+          ...product,
+        };
+        return updatedProducts;
+      }
+    });
+  }
+
+  function addProduct(newProduct: Product) {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  }
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
