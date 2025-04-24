f2a252f14893ed47730f93b6244d5fc562dc7f1c
hyunjin <71summernight@naver.com>
2025-04-24 23:36:42 +0900

    base url
    경로 바꾸기
    
    root
    
    path
    
    마지막 배포점 변경
    
    path
    
    원복
    
    tsconfig
    
    마지막 설정
    
    마지막 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
+import path from 'path';
 
 
 export default mergeConfig(
   defineConfig({
+    base: '/front_5th_chapter2-2/',
     plugins: [react()],
     build: {
       rollupOptions: {
         input: {
-          main: resolve(__dirname, 'index.refactoring.html'),
+          main: path.resolve(__dirname, 'index.refactoring.html'),
         },
       },
     },
