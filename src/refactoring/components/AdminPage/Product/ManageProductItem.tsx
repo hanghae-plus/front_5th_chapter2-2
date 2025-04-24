91fc8efc11ea6e4d347fa7a4bc414634de6ac121
hyunjin <71summernight@naver.com>
2025-04-24 23:32:05 +0900

    배포준비

--- src/refactoring/components/AdminPage/Product/ManageProductItem.tsx
+++ src/refactoring/components/AdminPage/Product/ManageProductItem.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function ManageProductItem() {
   return <div>ManageProductItem</div>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.refactoring.html'),
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
