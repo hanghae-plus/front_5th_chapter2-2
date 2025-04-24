a05bd808072580218ef8dab27835930ec16430ca
minhee <jmh3699@naver.com>
2025-04-24 17:09:43 +0900

    refactor: 할인 정보 수정 컴포넌트 분리 (`ProductEditDiscountForm`)

--- src/refactoring/components/ProductEditForm.tsx
+++ src/refactoring/components/ProductEditForm.tsx
@@ -1,6 +1,7 @@
 import { Product } from '../../types';
 import { useProductEdit } from '../hooks/useProductEdit';
 import { useDiscount } from '../hooks/useDiscount';
+import ProductEditDiscountForm from './ProductEditDiscountForm';
 
 interface Props {
   product: Product;
@@ -20,12 +21,6 @@ export default function ProductEditForm({
     handleStockUpdate,
     handleEditComplete,
   } = productEditor;
-  const {
-    newDiscount,
-    handleAddDiscount,
-    handleRemoveDiscount,
-    setNewDiscount,
-  } = productDiscounter;
 
   return (
     <div>
@@ -63,55 +58,11 @@ export default function ProductEditForm({
           className="w-full p-2 border rounded"
         />
       </div>
-      {/* 할인 정보 수정 부분 */}
-      <div>
-        <h4 className="text-lg font-semibold mb-2">할인 정보</h4>
-        {editingProduct?.discounts.map((discount, index) => (
-          <div key={index} className="flex justify-between items-center mb-2">
-            <span>
-              {discount.quantity}개 이상 구매 시 {discount.rate * 100}% 할인
-            </span>
-            <button
-              onClick={() => handleRemoveDiscount(product.id, index)}
-              className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
-            >
-              삭제
-            </button>
-          </div>
-        ))}
-        <div className="flex space-x-2">
-          <input
-            type="number"
-            placeholder="수량"
-            value={newDiscount.quantity}
-            onChange={(e) =>
-              setNewDiscount({
-                ...newDiscount,
-                quantity: parseInt(e.target.value),
-              })
-            }
-            className="w-1/3 p-2 border rounded"
-          />
-          <input
-            type="number"
-            placeholder="할인율 (%)"
-            value={newDiscount.rate * 100}
-            onChange={(e) =>
-              setNewDiscount({
-                ...newDiscount,
-                rate: parseInt(e.target.value) / 100,
-              })
-            }
-            className="w-1/3 p-2 border rounded"
-          />
-          <button
-            onClick={() => handleAddDiscount(product.id)}
-            className="w-1/3 bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-          >
-            할인 추가
-          </button>
-        </div>
-      </div>
+      <ProductEditDiscountForm
+        id={product.id}
+        editingProduct={editingProduct}
+        productDiscounter={productDiscounter}
+      />
       <button
         onClick={handleEditComplete}
         className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 mt-2"
