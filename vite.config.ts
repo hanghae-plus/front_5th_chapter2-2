6b82002e593ff6773191a4ff51da53da629e6887
chloekim <chloe@perfitt.io>
2025-04-24 18:27:17 +0900

    chore:배포 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,36 @@ import path from "path";
 import { defineConfig } from "vite";
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+  const BASE_PATH = isProd ? "/front_5th_chapter2-2/" : "/";
 
-    resolve: {
-      alias: {
-        "@refactoring": path.resolve(__dirname, "src/refactoring"),
+  return mergeConfig(
+    defineConfig({
+      base: BASE_PATH,
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@refactoring": path.resolve(__dirname, "src/refactoring"),
+        },
       },
-    },
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-    },
-  }),
-);
+      build: {
+        rollupOptions: {
+          input: {
+            "index.origin": path.resolve(__dirname, "index.origin.html"),
+            "index.refactoring": path.resolve(__dirname, "index.refactoring.html"),
+          },
+        },
+        outDir: "dist",
+      },
+    }),
+
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+      },
+    }),
+  );
+});
