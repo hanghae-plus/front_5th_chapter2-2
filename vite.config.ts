56acba7f1898fbac7827d7248e19ce839a2cd5c3
KumJungMin <rmawjdals@naver.com>
2025-04-24 21:55:02 +0900

    chore: 리팩토링 폴더 기준 절대 경로 지정하기

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
+
+import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/refactoring'),
+      },
+    },
   }),
+
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
-  })
-)
+  }),
+);
