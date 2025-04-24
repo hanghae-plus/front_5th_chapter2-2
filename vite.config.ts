023b80d6eee43535971a80dcac8509cd950109d9
Wonny <h.wonny11@gmail.com>
2025-04-25 04:15:10 +0900

    배포 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from "path";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: process.env.NODE_ENV === "production" ? "/front_5th_chapter2-2/" : "/",
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "index.html"),
+          basic: path.resolve(__dirname, "index.basic.html"),
+          advanced: path.resolve(__dirname, "index.advanced.html")
+        }
+      }
+    }
   }),
   defineTestConfig({
     test: {
