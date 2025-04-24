7f955663cc832490143af6da00c1d1697069dc00
maphnew <zcm3118@gmail.com>
2025-04-24 22:54:15 +0900

    🩹 fix: add key on loop

--- src/refactoring/components/AdminProductList.tsx
+++ src/refactoring/components/AdminProductList.tsx
@@ -1,17 +1,19 @@
 import { useProductsContext } from "../hooks/index.ts";
-import { AdminProduct } from "./AdminProduct.tsx"
+import { AdminProduct } from "./AdminProduct.tsx";
 
 export const AdminProductList = () => {
   const { products, updateProduct } = useProductsContext();
 
-  const getProductById = (productId: string) => products.find((p) => p.id === productId);
+  const getProductById = (productId: string) =>
+    products.find((p) => p.id === productId);
 
   return (
     <div className="space-y-2">
       {products.map((product, index) => (
-        <AdminProduct 
-          product={product} 
-          index={index} 
+        <AdminProduct
+          key={product.id}
+          product={product}
+          index={index}
           onProductUpdate={updateProduct}
           getProductById={getProductById}
         />
