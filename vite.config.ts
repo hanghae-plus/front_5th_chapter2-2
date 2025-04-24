0e9a9dd03bec36534e60b5d73f96545ef293a809
Jeong-wonho <kallzzang@naver.com>
2025-04-25 02:31:45 +0900

    ghpages 배포

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-export default mergeConfig(
-  defineConfig({
+export default defineConfig(({ mode }) => {
+  const base = mode === "production" ? "/front_5th_chapter2-2/" : "/";
+  
+  return {
+    base: base,
     plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+    build: {
+      outDir: 'dist',
     },
-  })
-)
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: ['/src/setupTests.js'],
+      include: [
+        'src/basic/**/*.test.js',
+        'src/advanced/**/*.test.js',
+      ],
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+      },
+    },
+  }
+})
