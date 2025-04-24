6529711a54e006be6fd7191a7bfe1f46dea51d93
devchaeyoung <chaemaa00@gmail.com>
2025-04-25 04:02:06 +0900

    chore: 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+const mode = process.env.NODE_ENV || 'development';
+const env = loadEnv(mode, process.cwd(), '');
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.refactoring.html'),
+        },
+      },
+    },
   }),
   defineTestConfig({
+    plugins: [react()],
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
   })
-)
+);
