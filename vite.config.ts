31fa0a3d53afa42d7ae8367282a66ea5f5cd4838
minhee <jmh3699@naver.com>
2025-04-25 00:14:48 +0900

    chore: 배포 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: '/front_5th_chapter2-2/',
+    build: {
+      outDir: './dist',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.refactoring.html'),
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
