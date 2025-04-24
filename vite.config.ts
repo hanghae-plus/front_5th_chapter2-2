affca6ab005604b73021b6cff1acd6bb3b168973
Jeong-wonho <kallzzang@naver.com>
2025-04-25 02:44:24 +0900

    fix : vite 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 // vite.config.js
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => {
   const base = mode === "production" ? "/front_5th_chapter2-2/" : "/";
@@ -10,6 +11,11 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     build: {
       outDir: 'dist',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.refactoring.html'),
+        },
+      },
     },
     test: {
       environment: 'jsdom',
