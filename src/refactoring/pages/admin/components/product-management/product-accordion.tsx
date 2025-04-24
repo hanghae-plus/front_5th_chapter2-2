05f67dd5e0ca168ce5710130a926101423161934
Doh Kim <ehgud456456@naver.com>
2025-04-24 20:26:12 +0900

    feat: view toggle 컴포넌트 생성

--- src/refactoring/pages/admin/components/product-management/product-accordion.tsx
+++ src/refactoring/pages/admin/components/product-management/product-accordion.tsx
@@ -5,6 +5,11 @@ import { useDiscounts } from "@r/model/discount/use-discounts";
 import { useProductContext } from "@r/model/product/product-context";
 import { Product } from "@r/model/product/types";
 import { Discount } from "@r/model/discount/types";
+import {
+  ToggleViewOnShow,
+  ViewToggle,
+  ViewToggleTrigger,
+} from "@r/shared/ui/view-toggle";
 
 interface ProductAccordionProps extends React.HTMLProps<HTMLDivElement> {
   product: Product;
@@ -26,13 +31,8 @@ export const ProductAccordion: React.FC<ProductAccordionProps> = ({
     product.discounts
   );
 
-  const [isOpen, setIsOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [newDiscount, setNewDiscount] = useState<Discount>(initialDiscount);
-
-  const toggleProductAccordion = () => {
-    setIsOpen((prev) => !prev);
-  };
 
   // 수정완료 버튼 클릭시 폼을 닫고, 상품 업데이트
   const handleEditComplete = () => {
@@ -52,131 +52,129 @@ export const ProductAccordion: React.FC<ProductAccordionProps> = ({
 
   return (
     <div className="bg-white p-4 rounded shadow" {...props}>
-      <button
-        data-testid="toggle-button"
-        onClick={toggleProductAccordion}
-        className="w-full text-left font-semibold"
-      >
-        {product.name} - {product.price}원 (재고: {product.stock})
-      </button>
-      {isOpen && (
-        <div className="mt-2">
-          {isEditing ? (
-            <div>
-              <div className="mb-4">
-                <label className="block mb-1">상품명: </label>
-                <input
-                  type="text"
-                  name="name"
-                  value={formValues.name}
-                  onChange={handleFormChange}
-                  className="w-full p-2 border rounded"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block mb-1">가격: </label>
-                <input
-                  type="number"
-                  value={formValues.price}
-                  name="price"
-                  onChange={handleFormChange}
-                  className="w-full p-2 border rounded"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block mb-1">재고: </label>
-                <input
-                  type="number"
-                  name="stock"
-                  value={formValues.stock}
-                  onChange={handleFormChange}
-                  className="w-full p-2 border rounded"
-                />
-              </div>
-              {/* 할인 정보 수정 부분 */}
+      <ViewToggle>
+        <ViewToggleTrigger
+          data-testid="toggle-button"
+          className="w-full text-left font-semibold"
+        >
+          {product.name} - {product.price}원 (재고: {product.stock})
+        </ViewToggleTrigger>
+        <ToggleViewOnShow>
+          <div className="mt-2">
+            {isEditing ? (
               <div>
-                <h4 className="text-lg font-semibold mb-2">할인 정보</h4>
-                {discounts.map((discount, index) => (
-                  <div
-                    key={index}
-                    className="flex justify-between items-center mb-2"
-                  >
+                <div className="mb-4">
+                  <label className="block mb-1">상품명: </label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={formValues.name}
+                    onChange={handleFormChange}
+                    className="w-full p-2 border rounded"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block mb-1">가격: </label>
+                  <input
+                    type="number"
+                    value={formValues.price}
+                    name="price"
+                    onChange={handleFormChange}
+                    className="w-full p-2 border rounded"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block mb-1">재고: </label>
+                  <input
+                    type="number"
+                    name="stock"
+                    value={formValues.stock}
+                    onChange={handleFormChange}
+                    className="w-full p-2 border rounded"
+                  />
+                </div>
+                {/* 할인 정보 수정 부분 */}
+                <div>
+                  <h4 className="text-lg font-semibold mb-2">할인 정보</h4>
+                  {discounts.map((discount, index) => (
+                    <div
+                      key={index}
+                      className="flex justify-between items-center mb-2"
+                    >
+                      <span>
+                        {discount.quantity}개 이상 구매 시 {discount.rate * 100}
+                        % 할인
+                      </span>
+                      <button
+                        onClick={() => handleRemoveDiscount(index)}
+                        className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
+                      >
+                        삭제
+                      </button>
+                    </div>
+                  ))}
+                  <div className="flex space-x-2">
+                    <input
+                      type="number"
+                      placeholder="수량"
+                      value={newDiscount.quantity}
+                      onChange={(e) =>
+                        setNewDiscount({
+                          ...newDiscount,
+                          quantity: parseInt(e.target.value),
+                        })
+                      }
+                      className="w-1/3 p-2 border rounded"
+                    />
+                    <input
+                      type="number"
+                      placeholder="할인율 (%)"
+                      value={newDiscount.rate * 100}
+                      onChange={(e) =>
+                        setNewDiscount({
+                          ...newDiscount,
+                          rate: parseInt(e.target.value) / 100,
+                        })
+                      }
+                      className="w-1/3 p-2 border rounded"
+                    />
+                    <button
+                      onClick={handleAddDiscount}
+                      className="w-1/3 bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+                    >
+                      할인 추가
+                    </button>
+                  </div>
+                </div>
+                <button
+                  onClick={handleEditComplete}
+                  className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 mt-2"
+                >
+                  수정 완료
+                </button>
+              </div>
+            ) : (
+              <div>
+                {product.discounts.map((discount, index) => (
+                  <div key={index} className="mb-2">
                     <span>
                       {discount.quantity}개 이상 구매 시 {discount.rate * 100}%
                       할인
                     </span>
-                    <button
-                      onClick={() => handleRemoveDiscount(index)}
-                      className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
-                    >
-                      삭제
-                    </button>
                   </div>
                 ))}
-                <div className="flex space-x-2">
-                  <input
-                    type="number"
-                    placeholder="수량"
-                    value={newDiscount.quantity}
-                    onChange={(e) =>
-                      setNewDiscount({
-                        ...newDiscount,
-                        quantity: parseInt(e.target.value),
-                      })
-                    }
-                    className="w-1/3 p-2 border rounded"
-                  />
-                  <input
-                    type="number"
-                    placeholder="할인율 (%)"
-                    value={newDiscount.rate * 100}
-                    onChange={(e) =>
-                      setNewDiscount({
-                        ...newDiscount,
-                        rate: parseInt(e.target.value) / 100,
-                      })
-                    }
-                    className="w-1/3 p-2 border rounded"
-                  />
-                  <button
-                    onClick={handleAddDiscount}
-                    className="w-1/3 bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-                  >
-                    할인 추가
-                  </button>
-                </div>
+                <button
+                  data-testid="modify-button"
+                  onClick={() => setIsEditing(true)}
+                  className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 mt-2"
+                >
+                  수정
+                </button>
               </div>
-              <button
-                onClick={handleEditComplete}
-                className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 mt-2"
-              >
-                수정 완료
-              </button>
-            </div>
-          ) : (
-            <div>
-              {product.discounts.map((discount, index) => (
-                <div key={index} className="mb-2">
-                  <span>
-                    {discount.quantity}개 이상 구매 시 {discount.rate * 100}%
-                    할인
-                  </span>
-                </div>
-              ))}
-              <button
-                data-testid="modify-button"
-                onClick={() => {
-                  setIsEditing(true);
-                  handleFormReset();
-                }}
-                className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 mt-2"
-              >
-                수정
-              </button>
-            </div>
-          )}
-        </div>
-      )}
+            )}
+          </div>
+        </ToggleViewOnShow>
+      </ViewToggle>
     </div>
   );
 };
---
+++ src/refactoring/shared/ui/view-toggle.tsx
@@ -0,0 +1,65 @@
+import { createContext, useContext, useState } from "react";
+
+interface ViewToggleContexttype {
+  isShow: boolean;
+  toggleView: () => void;
+}
+
+const ViewToggleContext = createContext<ViewToggleContexttype | null>(null);
+
+export const ViewToggle = ({ children }: { children: React.ReactNode }) => {
+  const [isShow, setIsShow] = useState(false);
+
+  const toggleView = () => setIsShow((prev) => !prev);
+
+  return (
+    <ViewToggleContext.Provider value={{ isShow, toggleView }}>
+      {children}
+    </ViewToggleContext.Provider>
+  );
+};
+
+const useViewToggleContext = () => {
+  const context = useContext(ViewToggleContext);
+  if (!context) {
+    throw new Error("useViewToggleContext must be used within a ViewToggle");
+  }
+  return context;
+};
+
+export const ToggleViewOnHide = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { isShow } = useViewToggleContext();
+
+  return !isShow && children;
+};
+
+export const ToggleViewOnShow = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { isShow } = useViewToggleContext();
+
+  return isShow && children;
+};
+
+interface ToggleTriggerProps extends React.ComponentProps<"button"> {
+  children: React.ReactNode;
+}
+
+export const ViewToggleTrigger = ({
+  children,
+  ...props
+}: ToggleTriggerProps) => {
+  const { toggleView } = useViewToggleContext();
+
+  return (
+    <button onClick={toggleView} {...props}>
+      {children}
+    </button>
+  );
+};
