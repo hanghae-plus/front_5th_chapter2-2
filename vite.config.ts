87f1de68a4ccfcb77f32e3fff651540cd335ff44
EZONE <meowoof01@gmail.com>
2025-04-24 14:22:40 +0900

    feat: 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+
+    build: {
+      rollupOptions: {
+        input: {
+          advanced: resolve(__dirname, 'index.refactoring.html'),
+        },
+      },
+    },
+    base: process.env.NODE_ENV === 'production' ? '/front_5th_chapter2-2/' : '/',
   }),
+
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
   })
-)
+);
