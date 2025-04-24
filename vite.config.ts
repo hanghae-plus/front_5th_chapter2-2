c09f959f82b23bc537442193eeb49ec5b27f0ca0
Jeong-wonho <kallzzang@naver.com>
2025-04-25 03:00:07 +0900

    fix: vite config2

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,28 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config"
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { resolve } from "path"
 
-export default defineConfig(({ mode }) => {
-  // 1. 현재 mode에 맞는 .env 파일을 불러옵니다.
-  //    세 번째 인자는 prefix 필터, ''로 두면 VITE_ 로 시작하는 모든 변수를 로드
-  const base = mode === "production" ? "/front_5th_chapter2-2/" : "/";
-  return {
-    // 2. base URL 설정
-    base: base,
-    plugins: [react()],
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'index.refactoring.html'),
-        },
-      },
-      outDir: 'dist',
-    },
+const mode = process.env.NODE_ENV || "development"
+const env = loadEnv(mode, process.cwd(), "")
 
-    test: {
-      environment: 'jsdom',
-      globals: true,
-      setupFiles: ['/src/setupTests.ts'],
-    },
-  }
-})
+export default mergeConfig(
+	defineConfig({
+		base: env.VITE_BASE_PATH,
+		plugins: [react()],
+		build: {
+			rollupOptions: {
+				input: {
+					main: resolve(__dirname, "index.refactoring.html"),
+				},
+			},
+		},
+	}),
+	defineTestConfig({
+		test: {
+			globals: true,
+			environment: "jsdom",
+			setupFiles: "./src/setupTests.ts",
+		},
+	})
+)
