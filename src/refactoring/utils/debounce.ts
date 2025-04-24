574fef32f7599f51c0e3aa07f73cd310cf27c4ee
LEEYEONSEONG <dustjd1535@gmail.com>
2025-04-24 21:20:58 +0900

    Add : debounce common util

---
+++ src/refactoring/utils/debounce.ts
@@ -0,0 +1,12 @@
+const debounce = <T extends (...args: any[]) => void>(
+  fn: T,
+  delay: number,
+): ((...args: Parameters<T>) => void) => {
+  let timer: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<T>) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delay);
+  };
+};
+
+export default debounce;
--- src/refactoring/utils/index.ts
+++ src/refactoring/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as getMaxDiscount } from "./getMaxDiscount";
 export { default as getRemainingStock } from "./getRemainingStock";
+export { default as debounce } from "./debounce";
