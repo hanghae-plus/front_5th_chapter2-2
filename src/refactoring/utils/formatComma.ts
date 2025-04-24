1d45ce521eadffecd37cdf920aaa8739cc43495f
hsw824 <ghdtjddn0824@naver.com>
2025-04-24 23:09:36 +0900

    fix: formatComma 함수 변경 (매개변수 string -> number)

--- src/refactoring/utils/formatComma.ts
+++ src/refactoring/utils/formatComma.ts
@@ -1,8 +1,3 @@
-export const formatComma = (price: string) => {
-  const priceNumber = Number(price);
-
-  if (isNaN(priceNumber)) {
-    return price;
-  }
-  return priceNumber.toLocaleString('ko-KR');
+export const formatComma = (price: number) => {
+  return price.toLocaleString('ko-KR');
 };
