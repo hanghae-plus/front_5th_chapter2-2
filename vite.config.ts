c8300e5abffc05d40d43c02d7d1eb876c7fdd6d9
DohyungKim <ehgud456456@naver.com>
2025-04-24 22:59:29 +0900

    ci: gh-pages 설치 및 deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,17 @@ import path from "path";
 
 export default mergeConfig(
   defineConfig({
+    base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter2-2/" : "/",
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          original: path.resolve(__dirname, "index.origin.html"),
+          refactoring: path.resolve(__dirname, "index.refactoring.html"),
+        },
+      },
+    },
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "src"),
