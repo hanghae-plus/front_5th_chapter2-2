bdfb5a28b17e96c27b2b921e3965e5a4abc89e3f
alchogh <alchogh01@gmail.com>
2025-04-25 01:43:19 +0900

    aad : 배포 설정 구현

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default mergeConfig(
   defineConfig({
+    base: '/front_5th_chapter2-2/',
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+        },
+      },
+      outDir: resolve(__dirname, 'dist'),
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
-  })
-)
+  }),
+);
